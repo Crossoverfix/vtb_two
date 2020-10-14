3ec882ef7f912324f3c5d0bacd03583df1aa4f94
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2020-10-14 12:03:33 +0300

    card animation + script

--- assets/js/business.js
+++ assets/js/business.js
@@ -1,16 +1,47 @@
 $(document).ready(function () {
-   var $scrollWindow = $('[data-js-scroll="window"]');
-   var $scrollContent = $('[data-js-scroll="content"]');
-   var $scrollTime = $('[data-js-scroll="time"]');
-   var $scrollControl = $('[data-js-scroll="control"]');
-   var $maxScrollValue = $scrollContent.outerWidth() - $scrollWindow.outerWidth();
-   var $scrollSize = $scrollWindow.outerWidth() / ($scrollContent.outerWidth() / 100)
-    $scrollControl.css('width',$scrollSize + '%');
-   var $maxScrollControl = $scrollControl.parent().outerWidth() - $scrollControl.outerWidth()
-    $scrollControl.on('mousedown',function () {
-        $scrollControl.on('mousemove',function () {
-            console.log('X=' + event.clientX + '  Y=' + event.clientY);
+    activeTab(1)
+    function activeTab(tabNumber) {
+        let controlLeft = $('[data-js-bs-tabs="'+ tabNumber +'"] .arr__left');
+        let controlRight = $('[data-js-bs-tabs="'+ tabNumber +'"] .arr__right');
+        let slides = $('[data-js-bs-tabs="'+ tabNumber +'"] .business-cards__content__slider__item');
+        let currSlide = 1;
+        let capSlide = slides.length;
+        controlCheck();
+        function controlCheck() {
+            if(currSlide > 1 && currSlide < capSlide){
+                controlLeft.removeClass('disable');
+                controlRight.removeClass('disable');
+            } else {
+                if(currSlide <= 1){
+                    controlLeft.addClass('disable');
+                }
+                if(currSlide >= capSlide){
+                    controlRight.addClass('disable');
+                }
+            }
+        }
+        controlLeft.on('click',function () {
+            if(!$(this).hasClass('disable')){
+                currSlide -= 1;
+                slides.removeClass('active');
+                slides.eq(currSlide - 1).addClass('active');
+                slides.eq(0).animate({'margin-left':'-'+ 50 * (currSlide - 1) +'%'},800);
+                controlCheck();
+
+            }
+            return false;
+        })
+        controlRight.on('click',function () {
+            if(!$(this).hasClass('disable')){
+                slides.removeClass('active');
+                slides.eq(currSlide).addClass('active');
+                slides.eq(0).animate({'margin-left':'-'+ 50 * currSlide +'%'},800);
+                currSlide += 1;
+                controlCheck();
+
+            }
+            return false;
         })
 
-    })
+    }
 })
